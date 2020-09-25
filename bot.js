8bd3b6db2bd7ed3e29508c799b8f9e4cf7c292a8
Shaffan Mustafa <shaffan1996@gmail.com>
2020-09-24 23:57:12 -0400

    bot works! has fact and moarfacts commands for the Fact Sphere

--- bot.js
+++ bot.js
@@ -1,45 +1,38 @@
 const discord = require('discord.js');
 const fs = require('fs');
-const _ = require('lodash');
+const {split, sample} = require('lodash');
+const lineReader = require('line-reader');
+
+require('dotenv').config();
+
 const TOKEN = process.env.TOKEN;
 const file = '/home/spacey/Desktop/parsedfacts.txt';
-const quotes = []; 
-const readline = require('readline')
 const bot = new discord.Client();
-require('dotenv').config();
-// this will help us read the file in a performant way
-// that would still be good practice even if this file
-// were much larger
-const readInterface = readline.createInterface({
-    input: fs.createReadStream(file),
-    output: process.stdout,
-    console: false
-});
 
-const getQuotes = (quoteFile) => {
-  readInterface.on('line', function(line) {
-    quotes.push(line);
-  });
-}
+// putting each line into its own element of our array 
+let text = fs.readFileSync(file, "utf-8");
+let quotes = text.split("\n");
 
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', async message => {
-  if (message.startsWith("!fact")) {
-    let randomQuote = _.sample(quotes);
-    await message.channel.send(randomQuote);
+bot.on('message', (message) => {
+  if (message.content.startsWith("!fact")) {
+    let randomQuote = sample(quotes);
+    message.channel.send(randomQuote);
   }
   // telling the bot not to reply to other bots
   if (message.author.bot) {
     return;
   }
-  if (message.startsWith("!moarfacts")) {
-    let factCount = _.split(message, " ");
+  if (message.content.startsWith("!moarfacts")) {
+    let factCount = split(message.content, " ")[1];
+    factCount = parseInt(factCount);
+    
     for (let i = 0; i < factCount; i++) {
-      await message.channel.send(_.sample(quotes));
+      message.channel.send(sample(quotes));
     }
   } 
 });
-bot.login('TOKEN');
+bot.login(TOKEN);
